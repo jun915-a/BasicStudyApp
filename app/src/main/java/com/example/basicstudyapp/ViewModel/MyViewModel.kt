a49cde7393fbf3642162533b52374984d753ff72
jarai00915 <jarai00915@gmail.com>
2023-11-23 01:25:39 +0900

    TODOコメント追記

--- app/src/main/java/com/example/basicstudyapp/ViewModel/MyViewModel.kt
+++ app/src/main/java/com/example/basicstudyapp/ViewModel/MyViewModel.kt
@@ -4,4 +4,10 @@ import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
     var myText = "Hello, Data Binding!"
+    //TODO:ライブデータ
+    //mutableLiveData<List<T>>
+
+    //TODO: モーションレイアウト
+
+    //TODO:jetpack compose
 }
